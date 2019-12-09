78397ff91a1490351de8a752e225779448d90557
Apple <appe@AppledeMacBook-Pro.local>
2019-12-09 10:20:41 -0800

    second version

--- JqueryExercise练习了jquery引入，jquery元素选择等等/exercise.js
+++ JqueryExercise练习了jquery引入，jquery元素选择等等/exercise.js
@@ -1,7 +1,16 @@
-//select all divs and give them a purple background
-$("div").css("background","purple");
-$(".highlight").css("width", "200px");
-$("#third").css("border", "2px solid orange");
-$("div:first-of-type").css("color","pink");
-$("div:first-of-type").css("background", "yellow");
-$("div:first").css("background", "red");
+
+$('button').on("click",function(){
+
+    $('div').slideToggle(1000,function () {
+        console.log("slide is done!");
+    });
+
+
+});
+
+
+
+
+
+
+
